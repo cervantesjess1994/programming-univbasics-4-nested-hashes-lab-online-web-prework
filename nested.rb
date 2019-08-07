62edf34d35d03896dc1c748a2013609e6a5d2550
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-07 08:36:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,6 +65,7 @@ end
 def adding_matz
 	hash.new = {:yukihiro_matsumoto}
 	yukihiro_matsumoto[:known_for => "Ruby", :languages => {}]
+	:languages["LISP", "C"]
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
